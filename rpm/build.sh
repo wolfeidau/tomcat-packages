ff0eec7f9b47b7b02e9959905851a6bc20a2f8bf
Mark Wolfe <mark@wolfe.id.au>
2011-09-08 22:54:28 +1000

    Mostly working need to add scripts and extra configuration files.

--- rpm/build.sh
+++ rpm/build.sh
@@ -5,4 +5,5 @@ rm -rf BUILD RPMS SRPMS tmp || true
 mkdir -p BUILD RPMS SRPMS
 
 # real action happens here
-rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/tmp" SPECS/tomcat-base.spec
+# rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/tmp" SPECS/tomcat-base.spec
+rpmbuild -bi --define="_topdir $PWD" --define="_tmppath $PWD/tmp" SPECS/tomcat-bamboo.spec
